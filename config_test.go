768f22391880c6bee0bf8e8e57a684f46ec06a78
Arthur Silva <arxdsilva@gmail.com>
2019-04-08 10:50:32 -0300

    c_test: unsets env vars on tests
    * Avoids cross testing usage of environmental variables

--- config_test.go
+++ config_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestLoad(t *testing.T) {
 	os.Setenv("PREST_CONF", "testdata/prest.toml")
+	defer os.Unsetenv("PREST_CONF")
 
 	Load()
 	if len(PrestConf.AccessConf.Tables) < 2 {
@@ -18,105 +19,101 @@ func TestLoad(t *testing.T) {
 	if !PrestConf.AccessConf.Restrict {
 		t.Error("expected true, but got false")
 	}
+
 }
 
 func TestParse(t *testing.T) {
 	os.Setenv("PREST_CONF", "testdata/prest.toml")
+
 	viperCfg()
 	cfg := &Prest{}
 	err := Parse(cfg)
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.HTTPPort != 6000 {
 		t.Errorf("expected port: 6000, got: %d", cfg.HTTPPort)
 	}
-
 	if cfg.PGDatabase != "prest" {
 		t.Errorf("expected database: prest, got: %s", cfg.PGDatabase)
 	}
 
 	os.Setenv("PREST_CONF", "../prest.toml")
 	os.Setenv("PREST_HTTP_PORT", "4000")
+
 	viperCfg()
 	cfg = &Prest{}
 	err = Parse(cfg)
-
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.HTTPPort != 4000 {
 		t.Errorf("expected port: 4000, got: %d", cfg.HTTPPort)
 	}
-
 	if !cfg.EnableDefaultJWT {
-		t.Error("expected true but got false")
+		t.Error("EnableDefaultJWT: expected true but got false")
 	}
 
 	os.Setenv("PREST_CONF", "")
-	os.Setenv("PREST_HTTP_PORT", "4000")
 	os.Setenv("PREST_JWT_DEFAULT", "false")
+
 	viperCfg()
 	cfg = &Prest{}
 	err = Parse(cfg)
-
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.HTTPPort != 4000 {
 		t.Errorf("expected port: 4000, got: %d", cfg.HTTPPort)
 	}
-
 	if cfg.EnableDefaultJWT {
-		t.Error("expected false but got true")
+		t.Error("EnableDefaultJWT: expected false but got true")
 	}
 
-	os.Setenv("PREST_HTTP_PORT", "4000")
+	os.Unsetenv("PREST_JWT_DEFAULT")
 	os.Setenv("PREST_CONF", "testdata/prest.toml")
+
 	viperCfg()
 	cfg = &Prest{}
 	err = Parse(cfg)
-
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.HTTPPort != 4000 {
 		t.Errorf("expected port: 4000, got: %d", cfg.HTTPPort)
 	}
 
+	os.Unsetenv("PREST_CONF")
+	os.Unsetenv("PREST_HTTP_PORT")
 	os.Setenv("PREST_JWT_KEY", "s3cr3t")
+
 	viperCfg()
 	cfg = &Prest{}
 	err = Parse(cfg)
-
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.JWTKey != "s3cr3t" {
 		t.Errorf("expected jwt key: s3cr3t, got: %s", cfg.JWTKey)
 	}
-
 	if cfg.JWTAlgo != "HS256" {
 		t.Errorf("expected (default) jwt algo: HS256, got: %s", cfg.JWTAlgo)
 	}
 
+	os.Unsetenv("PREST_JWT_KEY")
 	os.Setenv("PREST_JWT_ALGO", "HS512")
+
 	viperCfg()
 	cfg = &Prest{}
 	err = Parse(cfg)
-
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.JWTAlgo != "HS512" {
 		t.Errorf("expected jwt algo: HS512, got: %s", cfg.JWTAlgo)
 	}
+
+	os.Unsetenv("PREST_JWT_ALGO")
 }
 
 func TestGetDefaultPrestConf(t *testing.T) {
@@ -194,8 +191,8 @@ func TestDatabaseURL(t *testing.T) {
 }
 
 func TestHTTPPort(t *testing.T) {
-	os.Setenv("PREST_CONF", "")
 	os.Setenv("PORT", "8080")
+
 	viperCfg()
 	cfg := &Prest{}
 	err := Parse(cfg)
@@ -208,18 +205,19 @@ func TestHTTPPort(t *testing.T) {
 
 	// set env PREST_HTTP_PORT and PORT
 	os.Setenv("PREST_HTTP_PORT", "3000")
+
 	cfg = &Prest{}
 	err = Parse(cfg)
 	if err != nil {
 		t.Errorf("expected no errors, but got %v", err)
 	}
-
 	if cfg.HTTPPort != 8080 {
 		t.Errorf("expected http port: 8080, got: %d", cfg.HTTPPort)
 	}
 
 	// unset env PORT and set PREST_HTTP_PORT
 	os.Unsetenv("PORT")
+
 	cfg = &Prest{}
 	err = Parse(cfg)
 	if err != nil {
@@ -228,6 +226,8 @@ func TestHTTPPort(t *testing.T) {
 	if cfg.HTTPPort != 3000 {
 		t.Errorf("expected http port: 3000, got: %d", cfg.HTTPPort)
 	}
+
+	os.Unsetenv("PREST_HTTP_PORT")
 }
 
 func Test_parseDatabaseURL(t *testing.T) {
